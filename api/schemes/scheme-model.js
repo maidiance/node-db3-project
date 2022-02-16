26c7c3d1d51d474cb7cf57e82083085aa9fb3f5c
Winifred Lu <winnie.lu@comcast.net>
2022-02-16 15:18:35 -0500

    find model complete

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config.js');
+
 function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
@@ -15,6 +17,12 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+  return db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .groupBy('sc.scheme_id')
+    .orderBy('sc.scheme_id')
+    .count('st.step_id as number_of_steps')
+    .select('sc.scheme_id as scheme_id', 'sc.scheme_name')
 }
 
 function findById(scheme_id) { // EXERCISE B
